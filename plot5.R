7483bb7592cf68aa854a08a02c071ffdd2bc8e75
joser amon pardo <jrpardordgz@gmail.com>
2017-02-13 14:43:48 +0100

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,33 +1,12 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-# merge the two data sets 
-if(!exists("NEISCC")){
-  NEISCC <- merge(NEI, SCC, by="SCC")
-}
-
-library(ggplot2)
-
-# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
-
-# 24510 is Baltimore, see plot2.R
-# Searching for ON-ROAD type in NEI
-# Don't actually know it this is the intention, but searching for 'motor' in SCC only gave a subset (non-cars)
-subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
-
-
-
-png("plot5.png", width=840, height=480)
-g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
-g <- g + geom_bar(stat="identity") +
-  xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
-print(g)
-dev.off()
+> require(downloader)
+> dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+> download(dataset_url, dest = "data.zip", mode = "wb")
+> unzip("data.zip", exdir = "./")
+> NEI <- readRDS("summarySCC_PM25.rds")
+> SCC <- readRDS("Source_Classification_Code.rds")
+> data3 <- subset(NEI, fips == "24510" & type=="ON-ROAD")
+> baltimore2.sources <- aggregate(data3[c("Emissions")], list(type = data3$type, year = data3$year, zip = data3$fips), sum)
+> png("plot5.png")
+> qplot(year, Emissions, data = baltimore2.sources, geom= "line") + theme_gray() + ggtitle("Motor Vehicle-Related Emissions in Baltimore County") + xlab("Year") + ylab("Emission Levels")
+> dev.off()
+> qplot(year, Emissions, data = baltimore2.sources, geom= "line") + theme_gray() + ggtitle("Motor Vehicle-Related Emissions in Baltimore County") + xlab("Year") + ylab("Emission Levels")

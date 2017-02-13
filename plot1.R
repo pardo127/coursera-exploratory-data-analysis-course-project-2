b9505d86fafcd6bd0820cd6eb93cf5ece847eb39
joser amon pardo <jrpardordgz@gmail.com>
2017-02-13 13:51:49 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,16 +1,11 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
-# for each of the years 1999, 2002, 2005, and 2008.
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
-
-png('plot1.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
-dev.off()
+> require(downloader)
+> dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+> download(dataset_url, dest = "data.zip", mode = "wb")
+> unzip("data.zip", exdir = "./")
+> NEI <- readRDS("summarySCC_PM25.rds")
+> SCC <- readRDS("Source_Classification_Code.rds")
+> data2 <- with(NEI, aggregate(Emissions, by = list(year), sum))
+> png("Plot1.png")
+> plot(data2, type = "o", main = "Total PM2.5 Emissions", xlab = "Year", ylab = "PM2.5 Emissions", pch = 19, col = "red", lty = 6)
+> Dev.off()
+> plot(data2, type = "o", main = "Total PM2.5 Emissions", xlab = "Year", ylab = "PM2.5 Emissions", pch = 19, col = "red", lty = 6)

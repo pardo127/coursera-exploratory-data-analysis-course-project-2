0af559994b436240f39aba69374584759990de10
joser amon pardo <jrpardordgz@gmail.com>
2017-02-13 14:41:56 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,18 +1,14 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
 
-# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
-# Use the base plotting system to make a plot answering this question.
 
-subsetNEI  <- NEI[NEI$fips=="24510", ]
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
-
-png('plot2.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
-dev.off()
+> require(downloader)
+> dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+> download(dataset_url, dest = "data.zip", mode = "wb")
+> unzip("data.zip", exdir = "./")
+> NEI <- readRDS("summarySCC_PM25.rds")
+> SCC <- readRDS("Source_Classification_Code.rds")
+> subdata <- subset(NEI, fips == "24510")
+> baltimore <- tapply(subdata$Emissions, subdata$year, sum)
+> png("Plot2.png")
+> plot(baltimore, type = "o", main = "Total PM2.5 Emissions in Baltimore County", xlab = "Year", ylab = "PM2.5 Emissions", pch = 18, col = "red", lty = 5)
+> dev.off()
+> plot(baltimore, type = "o", main = "Total PM2.5 Emissions in Baltimore County", xlab = "Year", ylab = "PM2.5 Emissions", pch = 18, col = "red", lty = 5)
